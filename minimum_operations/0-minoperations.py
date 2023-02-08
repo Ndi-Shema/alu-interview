22db6cbc6a68b9e85bbfa153f5b817294fc10052
Ndi-Shema <f.shema@alustudent.com>
2023-02-08 23:16:33 +0200

    minimum operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -17,16 +17,19 @@ def minOperations(n):
     :param n:
     :return:
     '''
-    if n <= 0:
+    if type(n) is not int or n <= 0:
         return 0
-
     operations = 0
-    i = 2
-    while i <= n:
-        while n % i == 0:
-            operations += i
-            n = n / i
-        i += 1
-    operations += n
+    H = 1
+    copy_all = 0
+    paste = 0
+    H_copied = 0
+    while H < n:
+        if n % H == 0:
+            copy_all += 1
+            H_copied = H
+        paste += 1
+        operations = copy_all + paste
+        H += H_copied
     return operations
 print(minOperations.__doc__)
