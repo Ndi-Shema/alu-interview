99d483980c726c51ca622db300553911fa47e220
Ndi-Shema <f.shema@alustudent.com>
2023-02-09 13:09:09 +0200

    minimum operations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,22 +1,10 @@
-#!/usr/bin/python3
-"""
-  In a text file, there is a single character H.
-  Your text editor can execute
-  only two operations in this file:
-  Copy All and Paste. Given a number n,
-  write a method that calculates
-  the fewest number of operations needed to result
-  in exactly n H characters in the file.
-  """
-
-
 def minOperations(n):
-    # for n=0 or n=1
     if n <= 1:
         return 0
 
-    # loop to find prime factors (smaller number)
-    for i in range(2, int((n / 2) + 1)):
+    i = 2
+    while i <= n // 2:
         if n % i == 0:
-            return minOperations(int(n / i)) + i
+            return minOperations(n // i) + i
+        i += 1
     return n
--- minimum_operations/main.py
+++ minimum_operations/main.py
@@ -4,7 +4,8 @@ Main file for testing
 """
 
 minOperations = __import__('0-minoperations').minOperations
+minOperations1 = __import__('0-minoperations')
 n = 21
 print(minOperations(n))
 
-print(minOperations.__doc__)
+print(minOperations1.__doc__)
