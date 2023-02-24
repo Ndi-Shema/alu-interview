342ca80219b8c6302c0aef887278c1916e0b6f27
Ndi-Shema <f.shema@alustudent.com>
2023-02-24 15:02:04 +0200

    rain

---
+++ rain/0-rain.py
@@ -0,0 +1,20 @@
+def rain(walls):
+    n = len(walls)
+    if n == 0:
+        return 0
+
+    stack = []
+    total_water = 0
+
+    for i in range(n):
+        while stack and walls[i] > walls[stack[-1]]:
+            top = stack.pop()
+            if not stack:
+                break
+            distance = i - stack[-1] - 1
+            bounded_height = min(walls[i], walls[stack[-1]]) - walls[top]
+            total_water += distance * bounded_height
+
+        stack.append(i)
+
+    return total_water
