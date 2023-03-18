169ecff37243353b1d64af9b1c105054b78f86ba
Ndi-Shema <f.shema@alustudent.com>
2023-03-18 15:29:17 +0200

    pascal's triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+
 """
-    Return an empty list if n is less than or equal to 0
+    Generates a Pascal's Triangle with n rows.
+    Args:
+        n (int): The number of rows in the triangle.
+    Returns:
+        A list of lists representing the Pascal's Triangle with n rows.
 """
 def pascal_triangle(n):
     if n <= 0:
         return []
     triangle = [[1]]
     for i in range(1, n):
-        row = [0] * (i+1)
+        row = [0] * (i + 1)
         row[0] = row[-1] = 1
         for j in range(1, i):
-            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
     return triangle
