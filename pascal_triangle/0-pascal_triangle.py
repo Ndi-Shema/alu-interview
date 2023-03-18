9de7ce6258b13b2af7110a886e681644a0530fa5
Ndi-Shema <f.shema@alustudent.com>
2023-03-18 15:07:46 +0200

    pascal's triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """ Return an empty list if n is less than or equal to 0"""
+""" Return an empty list if n is less than or equal to 0
+    Initialize the triangle list with the first row
+    Generate each subsequent row using the previous row
+    Create a new row with i+1 elements, initially filled with 0
+    Set the first and last elements of the row to 1
+    Calculate the middle elements of the row based on the previous row
+    Append the completed row to the triangle list
+    Return the completed triangle list"""
 def pascal_triangle(n):
-
     if n <= 0:
         return []
 
-    # Initialize the triangle list with the first row
     triangle = [[1]]
 
-    # Generate each subsequent row using the previous row
     for i in range(1, n):
-        # Create a new row with i+1 elements, initially filled with 0
         row = [0] * (i+1)
-        # Set the first and last elements of the row to 1
         row[0] = row[-1] = 1
-        # Calculate the middle elements of the row based on the previous row
         for j in range(1, i):
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-        # Append the completed row to the triangle list
         triangle.append(row)
 
     return triangle
