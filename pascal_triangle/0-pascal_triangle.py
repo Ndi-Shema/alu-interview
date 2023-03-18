181b112ed3fbb53451161a203c323999b64d3cfb
Ndi-Shema <f.shema@alustudent.com>
2023-03-18 15:16:32 +0200

    pascal's triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -4,12 +4,10 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     triangle = [[1]]
-
     for i in range(1, n):
         row = [0] * (i+1)
         row[0] = row[-1] = 1
         for j in range(1, i):
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         triangle.append(row)
-
     return triangle
